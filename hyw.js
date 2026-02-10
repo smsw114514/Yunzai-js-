b1bffd5458fcbe66f7c6d8b9b91ff4f08e52db77
氏名氏无 <smsw1145@163.com>
2026-02-10 08:18:57 +0800

    Update hyw.js

--- hyw.js
+++ hyw.js
@@ -1,32 +1,36 @@
 import plugin from '../../lib/plugins/plugin.js'
 
-const repliedUsers = new Set()
+const cooldownMap = new Map()
+const COOLDOWN = 5 * 60 * 1000 // 5 分钟
 
-export class autoQuestionOnce extends plugin {
+export class autoQuestionCooldown extends plugin {
   constructor () {
     super({
-      name: '问号只回复一次',
-      dsc: '每个人发？只回复一次hyw',
+      name: '问号5分钟冷却',
+      dsc: '同一用户5分钟内只回复一次hyw',
       event: 'message',
       priority: 5000,
       rule: [
         {
           reg: /^[?？]$/,
-          fnc: 'replyOnce'
+          fnc: 'replyWithCooldown'
         }
       ]
     })
   }
 
-  async replyOnce (e) {
-    const uid = e.user_id
+  async replyWithCooldown (e) {
+    const key = e.user_id          // 只按人
+    // const key = `${e.group_id}_${e.user_id}` // ← 按“群+人”冷却（更常见）
 
-    // 已回复过，直接结束
-    if (repliedUsers.has(uid)) {
+    const now = Date.now()
+    const last = cooldownMap.get(key)
+
+    if (last && now - last < COOLDOWN) {
       return false
     }
 
-    repliedUsers.add(uid)
+    cooldownMap.set(key, now)
     await e.reply('hyw')
     return true
   }
